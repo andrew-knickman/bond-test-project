fa1d62c60a96de4c2aa94a3a1438285c2a183d3b
Andrew Knickman <andrewsknick@gmail.com>
2019-04-24 17:00:37 -0400

    Changed regex in JamesBond.java to improve test results (down to 25 assertion errors)

--- Project5/bond/JamesBond.java
+++ Project5/bond/JamesBond.java
@@ -1,6 +1,6 @@
 
 public class JamesBond {
 	public Boolean bondRegex(String input){
-		return input.matches("^\\([0-9]*007[0-9]*\\)$");
+		return input.matches("^[0-9,\\(]*\\([0-9]*007[0-9,\\)]*\\)[0-9]*");
 	}
 }
